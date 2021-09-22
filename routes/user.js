b8220c0dd46884286619f9764b777bf036985667
FrankBatista1 <fb53901@gmail.com>
2021-09-21 23:36:11 -0400

    Added some comments to review what I know

--- routes/user.js
+++ routes/user.js
@@ -7,16 +7,16 @@ const User = require('../models/User')
 const {generateJwt} = require('../middlewares/generateJWT')
 
 router.post('/signup', async(req,res) => {
-  const testEmail = await User.findOne({email: req.body.email});
-  if(testEmail) {
-    return res.status(500).json({mesasge: "Email already exist"})
+  const testEmail = await User.findOne({email: req.body.email});// gets the provided email in the db
+  if(testEmail) {// if exist
+    return res.status(500).json({mesasge: "Email already exist"})// Usually don't show that already exist for security purposes
   }
-  const userToCreate = await User.create(req.body);
+  const userToCreate = await User.create(req.body);// create() an object in the database with the object(req.body)
   try {
-    const salt = bycypt.genSaltSync();
-    userToCreate.password = bycypt.hashSync(req.body.password, salt);
-    userToCreate.save();
-    return res.status(201).json(userToCreate);
+    const salt = bycypt.genSaltSync();//iterations of ecnryption 
+    userToCreate.password = bycypt.hashSync(req.body.password, salt);// encrypts the password 
+    userToCreate.save();//pased to the database
+    return res.status(201).json(userToCreate);// shows the user created with the password encrypted
   } catch (error) {
     return res.status(500).json({message: "Couldn't create user"});
   }
